20a87c50f7ad10387a13d2ba58bafb9e510e53b7
mjkim <alswn8065@gmail.com>
2024-07-16 18:42:31 +0900

    merge request1111

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   console.log('4 commit')
   console.log('5 commit')
   console.log('6 commit')
+  console.log('7 commit')
   return (
     <div className="App">
       <header className="App-header">
