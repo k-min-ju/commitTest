8f1354ddd8a916b397ecfaf083b316de0225b11e
mjkim <alswn8065@gmail.com>
2024-07-16 18:33:53 +0900

    test3 commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   console.log('1 commit')
   console.log('2 commit')
+  console.log('3 commit')
   return (
     <div className="App">
       <header className="App-header">
